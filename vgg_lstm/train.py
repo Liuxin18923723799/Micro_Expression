f29ffa0091823b57bf3877310806ed81e9a2437a
thuongkhanh284 <thuongkhanh.tran284@gmail.com>
2019-02-25 14:52:58 +0200

    Add files via upload

---
+++ vgg_lstm/train.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import torchvision
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import math
+
+def prepare_data():
+
+
+def train_model_vgg():
+	# get the Model VGG
+
+
+def main():
+
+	print("Start to train ")
+
+if __name__ == '__main__':
+	main()
